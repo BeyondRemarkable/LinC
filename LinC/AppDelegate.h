eafa55da65a31d4f60c825de976a707467ce35b8
mikefan1991 <dudouzhu@foxmail.com>
2017-08-07 15:08:09 -0400

    Initial Commit

---
+++ LinC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LinC
+//
+//  Created by Yingwei Fan on 8/7/17.
+//  Copyright © 2017 BeyondRemarkable. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
