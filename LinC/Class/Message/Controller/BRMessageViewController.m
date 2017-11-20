942062c173ed7e8405df7a1599b064721d68cfb5
mikefan1991 <dudouzhu@foxmail.com>
2017-11-20 16:22:48 -0500

    bug fix

--- LinC/Class/Message/Controller/BRMessageViewController.m
+++ LinC/Class/Message/Controller/BRMessageViewController.m
@@ -1547,6 +1547,9 @@ typedef enum : NSUInteger {
 
 - (void)moreViewAudioCallAction:(BRChatBarMoreView *)moreView
 {
+    UIAlertController *alertController = [UIAlertController alertControllerWithTitle:NSLocalizedString(@"Prompt", nil) message:NSLocalizedString(@"The function is in the process of development", nil) preferredStyle:UIAlertControllerStyleAlert];
+    [alertController addAction:[UIAlertAction actionWithTitle:NSLocalizedString(@"OK", nil) style:UIAlertActionStyleDefault handler:nil]];
+    [self presentViewController:alertController animated:YES completion:nil];
     // Hide the keyboard
     [self.chatToolbar endEditing:YES];
     
@@ -1555,6 +1558,9 @@ typedef enum : NSUInteger {
 
 - (void)moreViewVideoCallAction:(BRChatBarMoreView *)moreView
 {
+    UIAlertController *alertController = [UIAlertController alertControllerWithTitle:NSLocalizedString(@"Prompt", nil) message:NSLocalizedString(@"The function is in the process of development", nil) preferredStyle:UIAlertControllerStyleAlert];
+    [alertController addAction:[UIAlertAction actionWithTitle:NSLocalizedString(@"OK", nil) style:UIAlertActionStyleDefault handler:nil]];
+    [self presentViewController:alertController animated:YES completion:nil];
     // Hide the keyboard
     [self.chatToolbar endEditing:YES];
     
