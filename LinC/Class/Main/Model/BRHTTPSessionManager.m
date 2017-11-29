0d7bf8c1c9917cab34218f061ef5fceada283619
zhe <zhewuwork@gmail.com>
2017-11-29 11:16:05 -0500

    merge

--- LinC/Class/Main/Model/BRHTTPSessionManager.m
+++ LinC/Class/Main/Model/BRHTTPSessionManager.m
@@ -16,7 +16,11 @@
     NSMutableSet *newSet = [NSMutableSet setWithSet:manager.responseSerializer.acceptableContentTypes];
     [newSet addObject:@"text/html"];
     manager.responseSerializer.acceptableContentTypes = newSet;
-    manager.requestSerializer.timeoutInterval = 30;
+    
+    [manager.requestSerializer willChangeValueForKey:@"timeoutInterval"];
+    manager.requestSerializer.timeoutInterval = 10.f;
+    [manager.requestSerializer didChangeValueForKey:@"timeoutInterval"];
+    
     return manager;
 }
 
