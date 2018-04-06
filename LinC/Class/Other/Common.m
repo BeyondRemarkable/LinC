323fd97ceae2a45103de688e1f41a2b8e6afd692
mikefan1991 <dudouzhu@foxmail.com>
2018-04-05 22:07:06 -0400

    修改域名 增加background语音通话

--- LinC/Class/Other/Common.m
+++ LinC/Class/Other/Common.m
@@ -15,7 +15,7 @@ NSString *const kLoginPasswordKey = @"loginPassword";
 NSString *const kLoginTokenKey = @"loginToken";
 NSString *const kLoginStatusKey = @"loginStatus";
 
-NSString *const kBaseURL = @"https://linc.beyondremarkable.com";
+NSString *const kBaseURL = @"https://known.beyondremarkable.com";
 NSString *const kVideoBaseURL = @"https://s3-ap-northeast-1.amazonaws.com/linc-app";
 
 NSString *const kServiceName = @"com.BeyondRemarkable.LinC";
